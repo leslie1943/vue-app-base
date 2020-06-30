980c7db8c5d4161f44d8373f660b4592f84c5e02
苏震 <suzhen@viewhigh.com>
2020-06-30 13:12:47 +0800

    暂时提交

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import './style.less'
 // import { HappyScroll } from 'vue-happy-scroll'
 // import 'vue-happy-scroll/docs/happy-scroll.css'
 import router from './router'
+const a = router
 
 Vue.config.productionTip = false
 
